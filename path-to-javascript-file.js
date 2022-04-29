79d9fc2a0aea17c661cd4b7576240c22602a40d4
Carlos <carlospinill@gmail.com>
2022-04-29 12:50:48 -0600

    y

--- path-to-javascript-file.js
+++ path-to-javascript-file.js
@@ -4,21 +4,76 @@ function startG(){ console.log('empezo!')
 
 let snakeH=new SnakeNode(4, null, 'R',width/2,width/2);
 let $apple=$('<div class="apple"></div>');
+ coord=getRandomAppleCoord(4);
+$apple.css({ left: coord[0], top: coord[1] });
+$stage.append($apple);
+let nodes=$('#stage').children('div');
+     let again=false;console.log(parseInt(nodes[3].style.left));
+     snakeH.direction='U';
+ setTimeout(function(){ move(snakeH, 500);
+     setTimeout(function(){move(snakeH,1000);},500);},500)
+     
+     //move(snakeH,1500);
+     //snakeH.direction='U';
+    // move(snakeH,2000);
 //should add an apple for the snake to eat
 
 //the snake should start moving, it should check that the head does not touch the borders of the stage and itself
 //whenever eats an apple the length of the snake should increase
 
 }
+function move(node, time){
+     if(node.direction ==='R'){ 
+          node.left+=15;
+          setTimeout(function(){node.$node.css({ left: node.left-7.5});}, time/2);
+          setTimeout(function(){node.$node.css({ left: node.left});}, time);
+          if(node.childNode){console.log(node.left ) 
+               if(node.childNode.left-node.left=== -30)node.childNode.direction='R';
+               if(node.childNode.top-node.top===15)node.childNode.direction='U';  
+               if(node.childNode.top-node.top=== -15)node.childNode.direction='D';
+               move(node.childNode, time) }}
+     if(node.direction ==='L'){ 
+          node.left-=15;
+          setTimeout(function(){node.$node.css({ left: node.left+7.5});}, time/2);
+          setTimeout(function(){node.$node.css({ left: node.left});}, time);
+          if(node.childNode){ 
+               if(node.childNode.left-node.left===30)node.childNode.direction='L';
+               if(node.childNode.top-node.top===15)node.childNode.direction='U';
+               if(node.childNode.top-node.top===-15)node.childNode.direction='D';
+               move(node.childNode, time);}}
+     if(node.direction ==='D'){ 
+          node.top+=15;
+          setTimeout(function(){node.$node.css({ top: node.top-7.5});}, time/2);
+          setTimeout(function(){node.$node.css({ top: node.top});}, time);
+          if(node.childNode){ 
+               if(node.childNode.left-node.left===15)node.childNode.direction='L';
+               if(node.childNode.left-node.left===-15)node.childNode.direction='R'; 
+               if(node.childNode.top-node.top=== -30)node.childNode.direction='D';
+               move(node.childNode, time);    }} 
+     if(node.direction ==='U'){ 
+          node.top-=15;
+          setTimeout(function(){node.$node.css({ top: node.top+7.5});}, time/2);
+          setTimeout(function(){node.$node.css({ top: node.top});}, time);
+          if(node.childNode){ 
+               if(node.childNode.left-node.left===15)node.childNode.direction='L';
+               if(node.childNode.left-node.left===-15)node.childNode.direction='R';  
+               if(node.childNode.top-node.top===30)node.childNode.direction='U';
+               move(node.childNode, time) }}                                                                              
+ }
 
 //this function should give a random ubication for the apple that the snake eats
 //its coordinates should not touch the snake body and should not be too close to the edges of the stage
-function getRandomAppleCoord(){
-     let ans=[width*Math.random(), width*Math.radom]
-     while(true){
-          
-
+//it is in the format [left,top]
+function getRandomAppleCoord(snakeL){
+     let ans=[(width*0.125) + (parseInt(width*0.75*Math.random()/15)*15), (width*0.125) + (parseInt(width*0.75*Math.random()/15)*15)];
+     let nodes=$('#stage').children('div');
+     let again=false;
+     for( let i=0; i<snakeL;i++){
+          if( Math.abs(parseInt(nodes[i].style.left) -ans[0])+Math.abs(parseInt(nodes[i].style.top) -ans[1])< 100)again=true;
      }
+     if(again)return getRandomAppleCoord(snakeL);
+
+    return ans; 
 }
 const $stage=$('#stage');
 const width=$stage.innerWidth();
@@ -28,23 +83,26 @@ function appendPiece(Node){
 }
 
 class SnakeNode{
-     constructor(len,parentN,dir, top, left){
+     constructor(len,parentN,dir, left, top){
           this.lenght=len;
+          
           this.direction=dir;
           this.parentNode=parentN;
           if(len===1)this.childNode=null;
-          else this.childNode=new SnakeNode(len -1,this,dir, top, left-10 );
+          else this.childNode=new SnakeNode(len -1,this,dir, left-15, top );
+          console.log(width);
           this.top=top;
           this.left=left;
           this.$node=$('<div class="snakeNode"></div>');
           this.$node.css({ left: this.left, top: this.top });
           $stage.append(this.$node);
+          return this;
           // this.setPosition(top,left)
           // appendPiece(this);
      }
 
-     setPosition(top,left){
-          this.$node.css({top,left});
+     setPosition(left,top){
+          this.$node.css({top : top,left:left});
      }
 
 }
